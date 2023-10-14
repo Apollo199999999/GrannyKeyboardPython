eb841dae918413fd7eed4f1aeb54285a2cf944bd
Matthias Wang <matthiaswang60@gmail.com>
2023-10-14 14:58:16 +0800

    Finish UI

--- granny_keyboard.py
+++ granny_keyboard.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+import tkinter.font as font
 from pynput import keyboard
 import random
 
 open_window = False
 
 def key_press(key):
-    # Indicate to spawn_window() that we should spawn a window
-    global open_window
-    open_window = True
+    if key is not keyboard.Key.backspace:
+        # Indicate to spawn_window() that we should spawn a window
+        global open_window
+        open_window = True
 
 def rgbtohex(r,g,b):
     return f'#{r:02x}{g:02x}{b:02x}'
@@ -28,9 +30,9 @@ def spawn_window():
             window["background"] = rgbtohex(r=r, g=g, b=b)
 
             # Generate random position
-            x = str(random.randint(200, window.winfo_screenwidth() - 200))
-            y = str(random.randint(200, window.winfo_screenheight() - 200))
-            window.geometry("300x200+{0}+{1}".format(x, y))
+            x = str(random.randint(100, window.winfo_screenwidth() - 100))
+            y = str(random.randint(100, window.winfo_screenheight() - 100))
+            window.geometry("300x270+{0}+{1}".format(x, y))
 
             # Draw a stretched Granny face
             # Create a canvas widget
@@ -43,15 +45,29 @@ def spawn_window():
             canvas.photo = background_image
             canvas.create_image((0,0), anchor="nw", image=background_image)
 
+            # Add "Granny is watching" label
+            lbl = tk.Label(window, text="G̷̛̗͉̬̫̠̰̣͖̫͓̘̥͖̭̖͖̮͙̺̯̣͖͉̦̮̳͇̲͔͊̔̄̀̃̏̔̊́̈́͠r̷̡͎̘͔͇͚̙̙̿̒͌̍̆̈́̌̽́͛͐̓̎̌̇̾̇͒̈́̋̀͛́̈́̓͌͊̑̑͋͒̆̎͒̕͘͜͝͠͝͝a̷̹̻̖̗̩͙̞̣͇̖̹̯̓̔̒͒̈̀͑̃̌̎͑̿͆̌ͅn̶̤̮̟̼̗̫̮̥̱̲̱͕̺̣̩͕̟̯̫̫͍̩͕͗͗̒̃̅͆̑͛̈͘̚n̸̨̨̡̳̖̦̘̝̫̺͇̥͚̝͚͙͖̦̬̜̠̬̟̩̅̈́́͐̊̾̒̅̂̋͌͊̈́̾̍̓̈́̽̊͋̕͝ͅͅy̴̧̛̛̛̼͉̟̰̘̹̖̬̬̦͌͐͊̐̃̀̾̉̈́̒̂̑́̈́̈́͋̀̆́̐̃͑͆̎̎͊̂͊́̽̉̉͂͆͊̉̎̂̾̀͗̚̚͝͝͠ͅ ̴̡͕̼̥̻̩̬̝̺̙̘̬̦̫͍̠̙͙͔̲́̿̈̿͂͆͆̂̂͌̋̓̄͂́̆̀͜͝͠ị̴̢̧̢̢̛̬̩͈̭͖̜̝̳̯̖͎̘͍̙͔̜͕̥̭͖̜͍̩̺̠̭̣͇̭̬͇̫͖̣̖̭͎̳͋͑̽̈́̌͑̃͐̃̈́̚̚͜͜͜͝͠ş̸̧̡̛̪̖̗̍̃́͒̇̉̕͜ͅ ̵͖̙͖̑̏͗̇̔͛̔̚͝w̶̛̖͈͉̟̩̯̖̘͇̭̼̖͈̱̪̌̾̈́̀͋̇̀̈́̔̓͛͊́̅̏̍̍̇̿̄̎͗̈̊̈́͗̀̔̓̿̆̓͂͑̍͘̚̚͘̚a̸̡̡̨̡̛̰͉̭̩̝̬͍͍̮̤̟͚̫͕͈̞̞̿͑̒̀̓́̀̉̔̑̀͗̈́͌̅̀̑͘͘̕̕͠t̷̛̛̛̫̝̳͙̹̓͆̀̈̈́̉̂̽̆̍͊̔̈́̓̿̈́͋̒̆̐̓͑͂͂̓̔́̇͑́̄̉̅̇̚͘͝c̸̨̡̨͇̻̲͎͍͇̲̼̬̫̣̰̻̙͈̯͖͚͇̠̲̤͉̫̣̬̞̑̀̎̇̍͗̂̏̏̎̓̓͋͐̈̊̇̓̅̆͗̈͗̌̏̎̀̿̌̓͘̕͜͜͜͝͝ͅḩ̷͕̙͉̞̞͓̳͖̗͚̞̪͉̮̙̩̯̞̲́̊̈́̏̓͋͆̔́̈̉̐̆͂́̅̀̓̔́͌̀̾͘͘͝͠ͅì̸̢̧̱͈͚̞͔̼̝͓̞̩̫̯̮̮̟̳̮̗̪̳̠̙̜͕̼̂̔̏̅͊̈́̓̎̐̀̉͆̉̀͆̾̓̾͛̎̿̄̊͌́̍̄́͆̄̔̔͊̒̀͛͋̍͒̄͜ň̸̨̨̧̪̫͎̱̟͎̭̦̤͚̬͎̹͖̰͔̲̞̯̯̦͉͓̪̈͑́̎͐͂̈́̈͋̽͊̊̈́̔͋͗̕͘͜͝ͅģ̸͉͈̰̜̰̩̬̰͇̮̟͔̼̦̤̩̌͋̔͐̈́̀̅̄̌̎̌̏̓̊̄̎̌̈̔̍͋̈́͂̿͗͋̉̃́̽͗̊̓̚̚", fg='red', font=("RansomNote", 20), bg=rgbtohex(r=r, g=g, b=b))
+            lbl.place(anchor="center", relx=0.5, y=160)
+
+            # Add buttons
+            acknowledge_btn = tk.Button(window, text="I acknowledge that Granny is my God", command=window.destroy)
+            acknowledge_btn.place(anchor="center", relx=0.5, y=205)
+            acknowledge_btn["font"] = font.Font(size=11)
+
+            no_btn = tk.Button(window, text=" No ")
+            no_btn.place(anchor="center", relx=0.5, y=245)
+            no_btn["state"] = "disabled"
+            no_btn["font"] = font.Font(size=11)
+
             open_window = False
             
-        # Update root no matter what so stuff doesnt freeze up
+        # Update root no matter what so stuff doesnt freeze
         root.update()
 
 
 
 listener = keyboard.Listener(
-    on_press=key_press,)
+    on_press=key_press)
 listener.start()
 
 root = tk.Tk()
